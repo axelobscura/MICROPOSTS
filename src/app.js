4188f947edb49bb7ceb98c4c3146090d84e553d4
Axel <axosar@gmail.com>
2018-02-28 18:09:24 -0600

    test3
    test3

--- src/app.js
+++ src/app.js
@@ -4,7 +4,8 @@ import { ui } from './ui';
 document.addEventListener('DOMContentLoaded', getPosts);
 
 function getPosts() {
-    http.get('http://localhost:3000/posts')
+    //http.get('http://localhost:3000/posts')
+    http.get('http://31.220.62.12/microposts/api/db.json')
         .then(data => ui.showPosts(data))
         .catch(err => console.log(err));
 }
