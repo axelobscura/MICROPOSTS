37dadf28674e125d3ed38979e20bdac3d2746830
Axel <axosar@gmail.com>
2018-02-28 18:07:04 -0600

    Ui Test
    Prueba de contenido

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ import { ui } from './ui';
 document.addEventListener('DOMContentLoaded', getPosts);
 
 function getPosts() {
-    http.get('http://31.220.62.12/microposts/api/db.json')
-        .then(data => console.log(data))
+    http.get('http://localhost:3000/posts')
+        .then(data => ui.showPosts(data))
         .catch(err => console.log(err));
 }
